88ad213585fe0ff7fb0ad0ee1c6398b25a8a1796
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-05-15 19:34:36 +0300

    Fix uncorrect vertical modal display, fixed jquery animations

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 $('.btn-callback.modal-open').on('click', function(e) {
   e.preventDefault();
   $('body').addClass(['darken', 'lock']);
-  $(this.dataset.target).show({});
+  $(this.dataset.target).show({ duration: 0, done: function() { $(this).addClass('is-open') } });
 })
 
 $('.btn-callback.modal-close').on('click', function(e) {
   e.preventDefault();
   $('body').removeClass(['darken', 'lock']);
-  $(this.dataset.target).hide({});
+  $(this.dataset.target).hide({ duration: 0, done: function() { $(this).removeClass('is-open') } });
 })
 
 $('.btn-callback.next-step').on('click', function(e) {
@@ -20,3 +20,10 @@ $('.btn-callback.next-step').on('click', function(e) {
     return;
   }
 })
+
+
+function radioChangeListener() {
+  console.log('changed');
+  $(this).parents('.quiz-step').find('button.next-step').prop('disabled', false);
+}
+$('.radio').on('change', radioChangeListener);
