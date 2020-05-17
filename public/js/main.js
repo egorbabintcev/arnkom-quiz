ca4ab81cbd7ab37ede9a65b146a807ee98a7996c
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-05-17 16:18:14 +0300

    Add leave contacts page, fix some styles, move progress bar styles to root of the css rules, add and minified images and svgs

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,12 @@ $('.btn-callback.next-step').on('click', function(e) {
   if (this.type !== 'submit') {
     e.preventDefault();
     const curStep = $(this).parents('.quiz-step');
+    if (curStep.hasClass('is-last')) {
+      curStep.parents('.quiz-form').find('.quiz-main , .quiz-sidebar').each((i, el) => $(el).prop('hidden', true));
+      curStep.parents('.quiz-form').find('.quiz-cta').prop('hidden', false);
+      curStep.removeClass('is-active');
+      return;
+    }
     const nextStep = curStep.next('.quiz-step');
     curStep.removeClass('is-active');
     nextStep.addClass('is-active');
@@ -25,5 +31,11 @@ $('.btn-callback.next-step').on('click', function(e) {
 function radioChangeListener() {
   console.log('changed');
   $(this).parents('.quiz-step').find('button.next-step').prop('disabled', false);
+  this.removeEventListener('change', radioChangeListener);
 }
 $('.radio').on('change', radioChangeListener);
+
+$('form.quiz-form').on('submit', function(e) {
+  e.preventDefault();
+  console.log($(this).serialize());
+})
