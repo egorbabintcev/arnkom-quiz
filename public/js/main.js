a1c6ef09abd1cd6f86ccd4419268b09d596aee8c
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-05-22 11:43:08 +0300

    Add some classes for js into html, change scripts for forwarding steps

--- public/js/main.js
+++ public/js/main.js
@@ -10,31 +10,19 @@ $('.btn-callback.modal-close').on('click', function(e) {
   $(this.dataset.target).hide({ duration: 0, done: function() { $(this).removeClass('is-open') } });
 })
 
-$('.btn-callback.next-step').on('click', function(e) {
-  if (this.type !== 'submit') {
-    e.preventDefault();
-    const curStep = $(this).parents('.quiz-step');
-    if (curStep.hasClass('is-last')) {
-      curStep.parents('.quiz-form').find('.quiz-main , .quiz-sidebar').each((i, el) => $(el).prop('hidden', true));
-      curStep.parents('.quiz-form').find('.quiz-cta').prop('hidden', false);
-      curStep.removeClass('is-active');
-      return;
-    }
-    const nextStep = curStep.next('.quiz-step');
-    curStep.removeClass('is-active');
-    nextStep.addClass('is-active');
-    return;
+$('button.next-step').on('click', function(evt) {
+  if (this.type === 'submit') return;
+
+  evt.preventDefault();
+  const curStep = $(this).parents('.quiz-step');
+  const nextStep = curStep.next('.quiz-step');
+  if (nextStep.hasClass('is-last')) {
+    curStep.parents('.quiz-form-wrapper').addClass('last-step');
   }
+  curStep.removeClass('is-active');
+  nextStep.addClass('is-active');
 })
 
-function radioChangeListener() {
-  console.log('changed');
-  $(this).parents('.quiz-step').find('button.next-step').prop('disabled', false);
-  this.removeEventListener('change', radioChangeListener);
-}
-$('.radio').on('change', radioChangeListener);
-
-$('form.quiz-form').on('submit', function(e) {
-  e.preventDefault();
-  console.log($(this).serialize());
-})
+$('.switch').on('change', function() {
+  $(this).parents('.quiz-step').find('button.next-step[disabled]').prop('disabled', false);
+});
