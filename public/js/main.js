b751004d7e405f3be2821e1a16346412d1caf80a
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-05-24 16:19:33 +0300

    Fix multiple checkbox value submit, add phone input mask and selection redefinition

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,18 @@
+$.fn.setCursorPosition = function(pos) {
+  this.each(function(index, elem) {
+    if (elem.setSelectionRange) {
+      elem.setSelectionRange(pos, pos);
+    } else if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  });
+  return this;
+};
+
 $('.btn-callback').on('click', function(e) {
   e.preventDefault();
   const { target, action } = this.dataset;
@@ -34,15 +49,24 @@ $('.switch').on('change', function() {
   $(this).parents('.quiz-step').find('button.next-step[disabled]').prop('disabled', false);
 });
 
-$('form input[name="url"]').prop('value', window.location.href);
 
-$('form').on('submit', function(evt) {
+$('form').on('submit', function() {
   const data = $(this).serialize();
-
   $.ajax({
     type: 'POST',
     url: '/php/submit.php',
     data,
     success: (res) => console.log(res),
   });
+})
+
+$('form input[name="url"]').prop('value', window.location.href);
+$('input[type="tel"]').mask('+7 ?(999) 999-99-99', { autoclear: false })
+$('input[type="tel"]').on('click', function(evt) {
+  const { selectionStart, value } = this;
+  const lastChar = value.match(/_/);
+
+  if (lastChar && selectionStart > lastChar.index) {
+    $(this).setCursorPosition(lastChar.index);
+  }
 })
