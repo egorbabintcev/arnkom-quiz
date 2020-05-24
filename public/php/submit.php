c139ced2fe9f82c73b62c5d8974b80c36c81c467
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-05-24 20:36:55 +0300

    Add full script for result submit, fix forms method attribute

--- public/php/submit.php
+++ public/php/submit.php
@@ -1,3 +1,47 @@
 <?php
 
-print_r($_POST);
+if (!isset($_POST)) exit();
+
+$mail_data = [];
+$fieldSet = [
+  "form_type" => "Тип формы:",
+  "phone_number" => "Номер телефона:",
+  "messenger" => "Способ связи:",
+  "rent_date" => "Дата аренды:",
+  "rent_square" => "Площадь помещения:",
+  "rent_floor" => "Этаж:",
+  "rent_time" => "Срок аренды:",
+  "rent_features" => "Дополнительные параметры:"
+];
+
+foreach ($_POST as $key => $value) {
+  $fieldSet_key = $fieldSet[$key];
+  if (gettype($value) == "array") {
+    $value = implode(', ', $value);
+  }
+  if (isset($fieldSet_key)) {
+    array_push($mail_data, $fieldSet_key . " " . urldecode($value));
+  } else {
+    array_push($mail_data, $key . " " . urldecode($value));
+  }
+}
+
+// Data for mail function
+$to = "e.e.babintsev@yandex.ru";
+$subject = "Заявка с сайта lp.arnkom.ru";
+$message = implode("\n", $mail_data);
+$headers = 'Content-type: text/plain; charset="utf-8"';
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "From: <noreply@arnkom.ru>\r\n";
+$headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+
+
+$mail = mail($to, $subject, $message, $headers);
+
+if ($mail) {
+  exit(header('HTTP/1.0 200 OK'));
+} else {
+  die(header("HTTP/1.0 500 Internal Server Error"));
+}
+
+?>
